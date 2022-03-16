8d68f42cae40fa46e2a5d173be32f8fcaf8c3b23
Valderhide <gpinsent@hotmail.com>
2022-03-15 23:12:57 -0230

    Continued progress with React Routers, but nothing is displayed. Issue with Localhost:3000?

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import { render } from "react-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import App from "./App";
+import Expenses from "./expenses";
+import Invoices from "./invoices";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(<App />, 
+<BrowserRouter>
+<Routes>
+<Route path= "/" element={<App />} />
+<Route path= "expenses" element={<Expenses />} />
+<Route path= "invoices" element={<Invoices />} />
+
+</Routes>
+</BrowserRouter>,
+
+rootElement
+);
