6409311ec21594b46656da27224d88f8a3138845
Valderhide <gpinsent@hotmail.com>
2022-03-18 13:08:11 -0230

    Quick post Coach meeting push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { render } from "react-dom";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Expenses from "./expenses";
 import Invoices from "./invoices";
@@ -7,34 +7,33 @@ import Invoice from "./invoice";
 
 const rootElement = document.getElementById("root");
 render(
-<BrowserRouter>
-<Routes>
-<Route path= "/" element={<App />}>
-<Route path= "expenses" element={<Expenses />} />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
 
-<Route path= "invoices" element={<Invoices />} >
-    <Route
-    index
-    element={
-        <main style ={{padding: "1rem"}}>
-            <p>Select an invoice</p>
-        </main>
-    }
-    />
-    <Route path =":invoiceId" element={<Invoice/>}/>    
-</Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
 
-<Route
-path ="*"
-element={
-    <main style ={{padding: "1rem"}}>
-        <p>There's nothing here!</p>
-    </main>
-}
-/>
-</Route>
-</Routes>
-</BrowserRouter>,
-
-rootElement
+  rootElement
 );
